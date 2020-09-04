2a386d91286f6124466a13410133bd57d5739064
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-04 22:08:12 +0900

    feat: error logic moved to error subclass

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -2,10 +2,17 @@ import { ValidationError } from 'express-validator';
 
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
     super();
 
     // due to extends build in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+  serializeErrors() {
+    return this.errors.map(err => {
+      return { message: err.msg, field: err.param };
+    })
   }
 }
