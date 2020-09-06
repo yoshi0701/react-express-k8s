7951fd3a1a62869f2ed82df83f80e8a539e1c4d1
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-06 16:19:21 +0900

    feat: mongoose type error revision

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose';
 
+// interface that describe the properties
+// required to create a new user
+
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// interface describe the properties
+// that a User model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -10,7 +24,15 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs)
+}
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
-export { User };
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+
+export { User, buildUser };
