399dacd923d28d97ac06de8949c1da80980f1933
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-08 22:51:53 +0900

    feat: k8s secret added

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -35,6 +37,19 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, 'asdf');
+
+    // Store it to session object
+    // avoid following code due to Type error
+    // req.session.jwt = userJwt
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
 
