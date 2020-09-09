3b419571fdd149baf6596a6ca13c7eb3c9ab8d8c
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-09 22:29:43 +0900

    feat: signout logic added

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -4,7 +4,9 @@ const router = express.Router();
 
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there');
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
