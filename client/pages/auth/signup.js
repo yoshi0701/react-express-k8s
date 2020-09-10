936fe819153ee3f9c3da18f23086b85c816e63a1
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-10 22:55:19 +0900

    feat: signup component logic updated

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    console.log(email, password);
-  }
-  return (
 
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email, password
+      });
+      console.log(response.data);
+    } catch(err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
+  return (
     <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
@@ -28,6 +38,16 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map(err =>
+              <li key={err.message}>{err.message}</li>
+            )}
+          </ul>
+      </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
