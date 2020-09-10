180801c799cd16c15c7281f3038ebf40c6e40133
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-10 22:03:23 +0900

    feat: client docker file prepared

--- auth/src/test/setup1.ts
+++ auth/src/test/setup1.ts
@@ -1,29 +1,29 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import { app } from '../app';
+// import { MongoMemoryServer } from 'mongodb-memory-server';
+// import mongoose from 'mongoose';
+// import { app } from '../app';
 
-let mongo: any;
-beforeAll(async () => {
-  mongo = new MongoMemoryServer();
-  const mongoUri = await mongo.getUri();
-
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useInifiedTopology: true
-  });
-});
-
-beforeEach(async () => {
-  const collections = await mongoose.connection.db.collection();
-
-  for (let collection of collections) {
-    await collection.deleyMany({});
-  }
-});
-
-afterAll(async () => {
-  await mongo.stop();
-  await mongoose.connection.close();
-})
+// let mongo: any;
+// beforeAll(async () => {
+//   mongo = new MongoMemoryServer();
+//   const mongoUri = await mongo.getUri();
+//
+//   // await mongoose.connect(mongoUri, {
+//   //   useNewUrlParser: true,
+//   //   useInifiedTopology: true
+//   // });
+// });
+//
+// beforeEach(async () => {
+//   const collections = await mongoose.connection.db.collection();
+//
+//   for (let collection of collections) {
+//     await collection.deleyMany({});
+//   }
+// });
+//
+// afterAll(async () => {
+//   await mongo.stop();
+//   await mongoose.connection.close();
+// })
 
 
---
+++ client/pages/banana.js
@@ -0,0 +1,3 @@
+export default () => {
+  return <h1>hogehoge</h1>;
+};
