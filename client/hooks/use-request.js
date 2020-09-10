4e7a0ff88a720d615fb7f4c7c4dd2c4935fb27a5
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-10 23:26:19 +0900

    feat: router via successful signup logic added

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
@@ -9,6 +9,11 @@ export default ({ url, method, body }) => {
       setErrors(null);
 
       const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
       return response.data;
     } catch (err) {
       setErrors(
@@ -20,7 +25,7 @@ export default ({ url, method, body }) => {
             )}
           </ul>
         </div>
-      )
+      );
     }
   };
 
