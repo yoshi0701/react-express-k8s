6d428f3a2687f466e887420ed2b27458b3e3a63e
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-10 23:39:00 +0900

    feat: LP update for signin logic via next SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+
+  console.log(currentUser);
+
   return <h1>landing pages</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get('/api/users/currentuser');
+
+  return response.data;
+};
+
+export default LandingPage;
