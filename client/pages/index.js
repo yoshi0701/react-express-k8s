15e69fa5ff4aa94e6f11a6b0554864541ba09295
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-10 23:44:13 +0900

    wip: draft

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 const LandingPage = ({ currentUser }) => {
 
   console.log(currentUser);
+  axios.get('/api/users/currentuser');
 
   return <h1>landing pages</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get('/api/users/currentuser');
 
-  return response.data;
-};
+
+// LandingPage.getInitialProps = async () => {
+//   const response = await axios.get('/api/users/currentuser');
+//
+//   return response.data;
+// };
 
 export default LandingPage;
