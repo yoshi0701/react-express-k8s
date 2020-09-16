4b540525f9672e8863ad94f8f6cc49da21926c41
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-09-17 00:21:56 +0900

    wip: minor revision

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,18 +2,38 @@ import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
 
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
   console.log(currentUser);
-  axios.get('/api/users/currentuser');
 
   return <h1>landing pages</h1>;
 };
 
 
 
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
-//
-//   return response.data;
-// };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on server
+    // request shoould be made to ingress-nginx.ingress...
+    const { data } = await axios.get(
+      'http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev'
+        }
+      }
+    );
+
+    return data;
+  } else {
+    // on browser
+    // request can be base URL
+    const { data } = await axios.get('/api/users/currentuser');
+    // { currentuser: {} }
+    return data;
+  }
+
+  return {};
+};
 
 export default LandingPage;
